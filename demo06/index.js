dbb7e32fb90fd583f3860c9918444549164e531d
陈情 <chenqing@cq.local>
2019-04-03 19:37:08 +0800

    generic

--- demo06/index.js
+++ demo06/index.js
@@ -7,3 +7,27 @@ var output = indentity('string');
 var output2 = indentity('mystring'); // most common 
 console.log(output);
 console.log(output2);
+// ### working with generic Types variables
+function indentity2(arg) {
+    console.log(arg.length);
+    return arg;
+}
+// other writing 
+function indentity3(arg) {
+    return arg;
+}
+var n = indentity2([1, 3]);
+var myIndentitiy = indentity3;
+var n2 = myIndentitiy([6, 6]);
+console.log(n2);
+var genericObj = indentity;
+// Generic class
+var GernericNumber = /** @class */ (function () {
+    function GernericNumber() {
+    }
+    return GernericNumber;
+}());
+var gernericNumber = new GernericNumber();
+gernericNumber.zeroNum = 5;
+var m = gernericNumber.add(5, 6);
+console.log(m);
--- demo06/index.ts
+++ demo06/index.ts
@@ -7,3 +7,31 @@ let output=indentity<string>('string');
 let output2=indentity('mystring');// most common 
 console.log(output);
 console.log(output2);
+// ### working with generic Types variables
+function indentity2<T>(arg:T[]):T[]{
+    console.log(arg.length);
+    return arg;
+}
+// other writing 
+function indentity3<T>(arg:Array<T>):Array<T>{
+    return arg;
+}
+let n=indentity2([1,3]);
+let myIndentitiy:<T>(arg:Array<T>)=>T[]=indentity3;
+let n2=myIndentitiy([6,6]);
+console.log(n2);
+// ## Generic Types function type and create generic interface
+// is not possible to create generic enum and generic namespaces
+interface GernericObj<T>{//可以非常明确的描述接口的参数
+    (arg:T):T
+}
+let genericObj:GernericObj<number>=indentity
+// Generic class
+class GernericNumber<T>{
+    zeroNum:T;
+    add:(x:T,y:T)=>T;
+}
+let gernericNumber=new GernericNumber<number>();
+gernericNumber.zeroNum=5;
+let m=gernericNumber.add(5,6);
+console.log(m);
